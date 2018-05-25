52e87219fba14022a5e5c46c2007241f1003420f
Michael R <gustavoholst@gmail.com>
2018-05-25 01:31:36 -0400

    Added beginning of form in PHP index file
    -Reorganized file
    -added sort type and sort date dropdowns
    
    TODO:
    -figure out PHP form submission and response of page while setting
    default selection to 'week'

--- www/index.php
+++ www/index.php
@@ -6,11 +6,41 @@
 <body>
   <p>Select from the following channels:</p>
   <?php
-  echo 'Hello World!';
-  ?>
-  <form name="userchoice" method="GET">
+    #Set variables
+    $logDir="D:/Documents/Websites/SXM/sxm-scrapy/sxm/channel_logs/";
+    $sortType = array('Top','New','Rising');
+    $sortDate = array('Hour','Day','Week','Month','Year','All');
 
-  	<input type="submit" value="Get Data">
-  </form
+    #Create array of log files from directory
+    $logFiles = array_filter(scandir($logDir), function($item) {
+      return !is_dir($logDir . $item);
+    });
+  ?>
+
+  <form name="sortForm" action="<?php echo htmlentities($_SERVER['PHP_SELF']); ?>" method="post">
+
+    <?php
+      #Create check boxes for each of the files listed in $logFiles
+      foreach ($logFiles as $filename){
+          echo "<input type=\"checkbox\" name=\"logfiles\" value=\"$filename\" /> $filename <br>";
+      }
+    ?>
+
+    <select name="sorttype">
+      <option value="top">Top</option>
+      <option value="new">New</option>
+      <option value="rising">Rising</option>
+    </select>
+    <select name="sortdate">
+      <option value="hour">Hour</option>
+      <option value="day">Day</option>
+      <option value="week">Week</option>
+      <option value="month">Month</option>
+      <option value="year">Year</option>
+      <option value="all">All</option>
+    </select>
+
+  	<br><input type="submit" value="Get Data">
+  </form>
 </body>
 </html>
